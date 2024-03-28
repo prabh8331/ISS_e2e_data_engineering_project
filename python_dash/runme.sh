61080bd025970a207cb1a20575335b744a682f97
prabh8331 <prabh8331@gmail.com>
2024-03-28 11:03:19 +0000

    test

--- python_dash/runme.sh
+++ python_dash/runme.sh
@@ -1,1 +1,8 @@
-pip3 install pandas folium dash
+# pip3 install pandas folium dash
+
+cd /home/userver/DataEngProj/ISS_e2e_data_engineering_project/python_dash/Containerized
+
+docker build -t dashboard-app .
+
+docker run -d --name dashboard-app --network ISS_proj -p 8050:8050 dashboard-app
+
